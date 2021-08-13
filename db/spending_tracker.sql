7063dfe86d4fb271be63aa9aacf1845fba639433
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-08-13 19:27:18 +0100

    spending_tracker database complete

---
+++ db/spending_tracker.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS merchants;
+DROP TABLE IF EXISTS tags;
+
+CREATE TABLE merchants (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(255)
+);
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    amount FLOAT,
+    date VARCHAR(255),
+    tag_id SERIAL REFERENCES tags(id),
+    merchant_id SERIAL REFERENCES merchants(id)
+)
