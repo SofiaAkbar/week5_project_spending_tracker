9fab504cb82820bcac64b11871c7c1a94662b373
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-08-14 17:00:17 +0100

    new_merchant route set up in controller

--- controllers/merchants_controller.py
+++ controllers/merchants_controller.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.merchant import Merchant
 import repositories.merchant_repository as merchant_repository
+import repositories.tag_repository as tag_repository
+import repositories.transaction_repository as transaction_repository
 
 merchants_blueprint = Blueprint("merchants", __name__)
 
@@ -9,4 +11,11 @@ merchants_blueprint = Blueprint("merchants", __name__)
 def merchants():
     merchants = merchant_repository.select_all()
     return render_template("merchants/index.html", merchants=merchants)
+
+# NEW MERCHANT
+@merchants_blueprint.route("/merchants/new", methods=['GET'])
+def new_merchant():
+    tags = tag_repository.select_all()
+    transactions = transaction_repository.select_all()
+    return render_template("merchants/new.html", tags=tags, transactions=transactions)
 
