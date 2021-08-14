10f11a57f8bf6f24321c57d7134a56af0fa22fdf
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-08-14 19:17:39 +0100

    created /tags route for tag controller

--- controllers/tags_controller.py
+++ controllers/tags_controller.py
@@ -6,3 +6,8 @@ import repositories.tag_repository as tag_repository
 import repositories.transaction_repository as transaction_repository
 
 tags_blueprint = Blueprint("tags", __name__)
+
+@tags_blueprint.route("/tags")
+def tags():
+    tags = tag_repository.select_all()
+    return render_template("tags/index.html", tags=tags)
